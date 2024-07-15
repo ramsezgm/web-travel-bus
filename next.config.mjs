d6cbecbfd1002e5edfd0ca0d5fc07ac9383ad8f0
Sh4cK-18 <auuz.451@gmail.com>
2024-07-15 13:51:54 -0500

    refactor: Update next.config.mjs to use ES modules syntax

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 // next.config.js
 
-module.exports = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://travel-bus-81kx.onrender.com/api/:path*', // Proxy to Backend
-      },
-    ];
-  },
-};
+export const reactStrictMode = true;
+export async function rewrites() {
+  return [
+    {
+      source: '/api/:path*',
+      destination: 'https://travel-bus-81kx.onrender.com/api/:path*', // Proxy to Backend
+    },
+  ];
+}
