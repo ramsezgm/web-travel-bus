15d524aa556302c88524d3335da887b2a5937474
Sh4cK-18 <auuz.451@gmail.com>
2024-07-15 07:53:16 -0500

    commit

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -14,7 +14,7 @@ const Sidebar = () => {
   const handleLogout = async () => {
     document.cookie = 'token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
     localStorage.removeItem('userId');
-    router.push('/auth');
+    router.push('/');
   };
 
   const getLinkStyle = (path) => {
